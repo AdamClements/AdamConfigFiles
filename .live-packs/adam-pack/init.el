e37555afaae96c2d208213c5259ac346a74c926c
Adam Clements <adam.clements@gmail.com>
2012-09-23 20:56:47 +0100

    Transparent emacs

--- .live-packs/adam-pack/init.el
+++ .live-packs/adam-pack/init.el
@@ -23,3 +23,7 @@
                 :width extra-condensed
                 :foundry "unknown"
                 :family "DejaVu Sans Mono")))))
+
+
+(set-frame-parameter (selected-frame) 'alpha '(85 85))
+(add-to-list 'default-frame-alist '(alpha 85 85))
