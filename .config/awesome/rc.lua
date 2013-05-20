2d084c146894d3dbc8aaf9fe610c9e807593cd0f
Adam Clements <adam.clements@gmail.com>
2013-05-20 11:33:42 +0100

    Add in close window key for computers without XF86Close button

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -115,6 +115,7 @@ root.keys(globalkeys)
 clientkeys = awful.util.table.join(
     awful.key({ modkey,           }, "f",         function (c) c.fullscreen = not c.fullscreen  end),
     awful.key({                   }, "XF86Close", function (c) c:kill()                         end),
+    awful.key({ modkey, "Shift"   }, "c",         function (c) c:kill()                         end),
     awful.key({ modkey, "Control" }, "space",     awful.client.floating.toggle                     ),
     awful.key({ modkey, "Control" }, "Return",    function (c) c:swap(awful.client.getmaster()) end),
     awful.key({ modkey,           }, "o",         awful.client.movetoscreen                        ),
